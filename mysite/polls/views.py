1e15730db57ed5cdd2a1ca3d4e9ca1ca349e32e6
Emmanuel <emmanuel.dacquah.gmail.com>
2022-10-02 19:59:27 +0300

    I have added a new directory called template/polls to the polls app and added an index.html file to it

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,24 @@
+from re import template
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Question
+
 
 # Create your views here.
 def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        "latest_question_list" : latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request):
+    return HttpResponse("Hello, World. You're at the polls index")
+
+
+def results(request):
+    return HttpResponse("Hello, World. You're at the polls index")
+
+
+def vote(request):
     return HttpResponse("Hello, World. You're at the polls index")
